5ae6874d482e1ada384ee1a244fb23ba29f1702e
Bram <84628826+sys-256@users.noreply.github.com>
2022-05-16 21:40:55 +0200

    improve typings (accordings to docs query isnt used)

--- src/index.js
+++ src/index.js
@@ -35,9 +35,8 @@ module.exports = class CatboyClient {
     constructor() {
         const baseURL = `https://api.catboys.com/`;
         Object.keys(endpoints).forEach(async (endpoint) => {
-            this[endpoint] = async (queryParams = ``) => {
+            this[endpoint] = async () => {
                 let url = new URL(`${baseURL}${endpoints[endpoint]}`);
-                if (queryParams !== ``) url.search = new URLSearchParams(queryParams);
                 return await getContent(url.toString());
             };
         });
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,51 +1,64 @@
-// List all the functions
 declare class CatboyClient {
     /**
-     * Image
-     * @returns {Promise<CatboyClient.CatboyRequestResults>} A JSON object containing the image URL
+     * Return a random catboy image
+     * @returns {Promise<CatboyClient.CatboyImageResults>} A JSON object containing the image URL
      */
-    image(): Promise<CatboyClient.CatboyRequestResults>;
+    image(): Promise<CatboyClient.CatboyImageResults>;
     /**
-     * Baka
-     * @returns {Promise<CatboyClient.CatboyRequestResults>} A JSON object containing the image URL
+     * Returns a random baka gif
+     * @returns {Promise<CatboyClient.CatboyBakaResults>} A JSON object containing the image URL
      */
-    baka(): Promise<CatboyClient.CatboyRequestResults>;
+    baka(): Promise<CatboyClient.CatboyBakaResults>;
     /**
-     * Response
+     * Returns a random saying from a virtual catboy
      * @returns {Promise<CatboyClient.CatboyChatResults>} A JSON object containing the chat response
      */
     response(): Promise<CatboyClient.CatboyChatResults>;
     /**
-     * 8Ball
-     * @param {CatboyClient.CatboyQueryParams} options A JSON object containing the options
-     * @returns {Promise<CatboyClient.CatboyChatResults>} A JSON object containing the 8Ball response and matching image URL
+     * Returns a random saying from a virtual 8ball
+     * @returns {Promise<CatboyClient.Catboy8BallResults>} A JSON object containing the 8Ball response and matching image URL
      */
-    "8Ball"(options: CatboyClient.CatboyQueryParams): Promise<CatboyClient.CatboyChatResults>;
+    "8Ball"(): Promise<CatboyClient.Catboy8BallResults>;
     /**
-     * Dice
-     * @param {CatboyClient.CatboyQueryParams} options A JSON object containing the options
-     * @returns {Promise<CatboyClient.CatboyChatResults>} A JSON object containing the dice roll and matching image URL
+     * Returns a random number and image from a virtual 6-sided dice
+     * @returns {Promise<CatboyClient.CatboyDiceResults>} A JSON object containing the dice roll and matching image URL
      */
-    dice(options: CatboyClient.CatboyQueryParams): Promise<CatboyClient.CatboyChatResults>;
+    dice(): Promise<CatboyClient.CatboyDiceResults>;
 }
 
 declare namespace CatboyClient {
-    // Help create options interface for the few functions that need it
     export interface CatboyQueryParams {
         text: string;
     }
 
-    export interface CatboyRequestResults {
+    export interface CatboyImageResults {
         url: string;
+        artist: string;
+        artist_url: string;
+        source_url: string;
+        error: string;
+    }
+
+    export interface CatboyBakaResults {
+        url: string;
+        error: string;
     }
 
     export interface CatboyChatResults {
         response: string;
-        url?: string;
+        error: string;
     }
 
-    export interface CatboyCatResult {
-        catboy: string;
+    export interface Catboy8BallResults {
+        response: string;
+        url: string;
+        error: string;
+    }
+
+    export interface CatboyDiceResults {
+        response: string;
+        url: string;
+        error: string;
     }
 }
 
